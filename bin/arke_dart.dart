8b8706de3e9762bd54c6482bd0cec2608c572c6c
downtomarsguy <downtomarsguy@gmail.com>
2025-04-15 16:40:38 +0530

    Test Command Added: Pinger

--- bin/arke_dart.dart
+++ bin/arke_dart.dart
@@ -1,15 +1,36 @@
-import 'dart:io';
-import 'package:nyxx/nyxx.dart';
-import 'package:dotenv/dotenv.dart';
+import "dart:io";
+import "package:nyxx/nyxx.dart";
+import "package:dotenv/dotenv.dart";
+import "package:nyxx_commands/nyxx_commands.dart";
 
 void main() async {
   var env = DotEnv(includePlatformEnvironment: true)..load();
-  var token = env['API_KEY']!;
+  var token = env["API_KEY"]!;
+  var guild_id = env["GUILD_ID"]!;
+
+  CommandsPlugin commands = CommandsPlugin(
+    prefix: (message) => "!",
+    guild: Snowflake.parse(guild_id),
+    options: CommandsOptions(logErrors: true),
+  );
 
   final client = await Nyxx.connectGateway(
     token,
-    GatewayIntents.allUnprivileged,
+    GatewayIntents.allUnprivileged | GatewayIntents.guildMembers,
+    options: GatewayClientOptions(
+      plugins: [commands, logging, cliIntegration, ignoreExceptions],
+    ),
   );
 
   stdout.writeln("🟢 Bot Booted Online");
+
+  ChatCommand pinger = ChatCommand(
+    "pinger",
+    "Checks if the bot is online",
+    id("pinger", (ChatContext context) {
+      context.respond(MessageBuilder(content: "sup nerds"));
+    }),
+  );
+
+  commands.addCommand(pinger);
 }
